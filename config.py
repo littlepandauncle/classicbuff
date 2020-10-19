5e5dbef9b843d2f65253d0485028c7a495836035
littlepandauncle <53169994+littlepandauncle@users.noreply.github.com>
2020-10-19 17:03:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+# 坐标 pixel
+# 聊天框像素范围
+chat_range = (40, 735, 540, 1020)
+# 登录界面大号吃buff角色像素范围
+role1_range = (1575, 116, 1890, 185)
+# 登录界面小号蹲buff角色像素范围
+role2_range = (1575, 196, 1890, 265)
+# 登录界面登陆按钮像素范围
+enter_range = (840, 975, 1070, 1010)
+# 掉线提示像素范围
+offline_range = (760, 490, 1160, 525)
+
+# 登录界面大号吃buff角色像素点
+role1_pos = (1732, 150)
+# 登录界面小号蹲buff角色像素点
+role2_pos = (1732, 210)
+# 登录界面登陆按钮像素点
+enter_pos = (955, 992)
+
+# 命中的字符
+haka_string = '莫托尔'
+heilong_string = '伦萨克'
+naifa_string = '萨鲁法尔'
+qiuzhang_string = '黑手'
+
+# 延时定义
+haka_delay = 40
+longtou_delay = 25
+qiuzhang_delay = 25
+afk_delay_min = 600
+afk_delay_max = 1200
